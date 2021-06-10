ca3b9b64f1d5138b13111588000e24bfff07f010
Freddy2296 <Garciafreddy328@yahoo.com>
2021-06-10 20:15:31 +0000

    created cards array

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var cardsArray = [
+    {
+      'name': 'pickle rick',
+      'img': 'img/pickle.rick.png',
+    },
+    {
+      'name': 'morty',
+      'img': 'img/morty.PNG',
+    },
+    {
+      'name': 'jerry',
+      'img': 'img/jerry.smith.PNG',
+    },
+    {
+      'name': 'rick',
+      'img': 'img/rick.sanchez.png',
+    },
+    {
+      'name': 'scary terry',
+      'img': 'img/scary.terry.PNG',
+    },
+    {
+      'name': 'giant head',
+      'img': 'img/giant.head.PNG',
+    },
+    {
+      'name': 'crazy rick',
+      'img': 'img/crazy.rick.PNG',
+    },
+    {
+      'name': 'Mr.m',
+      'img': 'img/mr.meeseeks.png',
+    },
+    {
+      'name': 'Mr.pb',
+      'img': 'img/Mr.pottybutthole.PNG',
+    },
+    {
+      'name': 'bird person',
+      'img': 'img/bird.person2.PNG',
+    },
+    {
+      'name': 'summer',
+      'img': 'img/summer.PNG',
+    },
+    {
+      'name': 'beth',
+      'img': 'img/beth1.png',
+    },
+  ]
+ 
