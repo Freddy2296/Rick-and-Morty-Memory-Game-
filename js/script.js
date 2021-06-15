2fe2fdbb82331436cda3c85b988edae52555ed7e
Freddy2296 <Garciafreddy328@yahoo.com>
2021-06-15 00:37:24 +0000

    modified js

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ var cardsArray = [
       'name': 'beth',
       'img': 'img/beth1.png',
     },
-  ]
+  ];
  
 
 
@@ -222,40 +222,4 @@ function startGame (){
    clearInterval(interval);
 }
 
-//modal
-let modal = document.getElementById("popup1")
-//stars list
- let starsList = document.querySelectorAll(".stars li");
-//close icon in modal
- let closeicon = document.querySelector(".close");
-//congratulations when all cards match, show modal and moves, time and rating
-function congratulations(){
-    //if (matchedCard.length == 12){
-    if (matchs == 12){
-        clearInterval(interval);
-        finalTime = timer.innerHTML;
-    //show congratulations modal
-    modal.classList.add("show");
-    //declare star rating variable
-    var starRating = document.querySelector(".stars").innerHTML;
-    //showing move, rating, time on modal
-    document.getElementById("finalMove").innerHTML = moves;
-    document.getElementById("starRating").innerHTML = starRating;
-    document.getElementById("totalTime").innerHTML = finalTime;
-    //closeicon on modal
-    closeModal();
-    }
-}
 
-//close icon on modal
-function closeModal(){
-  closeicon.addEventListener("click", function(e){
-      modal.classList.remove("show");
-      startGame();
-  });
-}
-//for player to play Again 
-function playAgain(){
-  modal.classList.remove("show");
-  startGame();
-}
